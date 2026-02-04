9ac126919159c7e70455ee0ba3b00d01a461dbdf
CedricKernec <cedrickernec.ck@gmail.com>
2026-02-04 12:35:20 +0100

    feat(dahsboard): Integration de l'API pour l'affichage des réservations en cours et à venir dans le dashboard

--- src/web/controllers/dashboardController.js
+++ src/web/controllers/dashboardController.js
@@ -6,33 +6,39 @@
  * - Liste les réservations en cours et à venir
  * - Tolère les erreurs de chargement partiel
  */
-
-import Reservation from "../../api/models/Reservation.js";
 import { formatDateLongFR } from "../utils/dateFormatter.js";
 import { mapReservationToDashboard } from "../utils/reservations/reservationDashboardMapper.js";
+import { fetchReservations } from "../services/api/reservationApi.js";
+import { handleAuthExpired } from "../middlewares/authExpiredHandler.js";
 
 export const getDashboard = async (req, res, next) => {
     let reservations = [];
     let dashboardError = null;
 
     try {
-        // ========================================================
-        // DATE DU JOUR FORMATÉE
-        // ========================================================
 
         const now = new Date();
         const formattedToday = formatDateLongFR(now);
 
-        // ========================================================
-        // RÉSERVATIONS EN COURS / À VENIR
-        // ========================================================
-
         try {
-            const rawReservations = await Reservation.find({
-                endDate: { $gte: now },
-            }).sort({ startDate: 1 });
+            const apiData = await fetchReservations(req, res);
 
-            reservations = rawReservations.map(mapReservationToDashboard);
+            if (handleAuthExpired(apiData, req, res)) return;
+
+            if (!apiData) {
+                throw new Error("apiData est undefined ou null");
+            }
+
+            if (!apiData.success) {
+                return next (new Error(apiData?.message || COMMON_MESSAGES.SERVER_ERROR_LONG));
+            }
+
+            const allReservations = apiData.data;
+            const upcomingReservations = allReservations
+                .filter(r => new Date(r.endDate) >= now)
+                .sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+
+            reservations = upcomingReservations.map(mapReservationToDashboard);
 
         } catch (error) {
             console.error(
@@ -41,10 +47,6 @@ export const getDashboard = async (req, res, next) => {
             );
             dashboardError = "Impossible de charger les réservations.";
         }
-
-        // ========================================================
-        // RENDER
-        // ========================================================
 
         res.render("dashboard", {
             title: "Dashboard - API Russell",
