24f02971531edf8915b7f747c5054ca86d9ba801
CedricKernec <cedrickernec.ck@gmail.com>
2026-02-25 21:08:19 +0100

    feat(api): ajout JSDOC sur la database

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -16,6 +16,18 @@
 
 import mongoose from "mongoose";
 
+/**
+ * Initialise la connexion MongoDB via Mongoose.
+ *
+ * - Utilise la variable d'environnement MONGO_URI
+ * - Log le statut de connexion
+ * - Stoppe le processus en cas d'échec critique
+ *
+ * @async
+ * @function connectDB
+ *
+ * @returns {Promise<void>}
+ */
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
